868699f7503cba412b16737348ee4945b121aeb1
tiwariaditya-cu <55505615+tiwariaditya-cu@users.noreply.github.com>
2019-09-19 01:37:10 +0530

    Add files via upload

---
+++ experiment 2/2-code.ino
@@ -0,0 +1,31 @@
+void setup() {
+  pinMode(1,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  
+
+}
+
+void loop() {
+  digitalWrite(1,HIGH);
+  digitalWrite(1,LOW);
+  delay(600);
+  digitalWrite(2,HIGH);
+  digitalWrite(2,LOW);
+  delay(600);
+  digitalWrite(3,HIGH);
+  digitalWrite(3,LOW);
+  delay(600);
+  digitalWrite(4,HIGH);
+  digitalWrite(4,LOW);
+  delay(600);
+  digitalWrite(5,HIGH);
+  digitalWrite(5,LOW);
+  delay(600);
+  digitalWrite(6,HIGH);
+  digitalWrite(6,LOW);
+  delay(100);
+}
